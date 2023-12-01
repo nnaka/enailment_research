7ccc2e348ac092cee00a9fec728d56ce7f14d3f0
Norihito Naka <nn1331@log-1.hpc.nyu.edu>
2023-12-01 10:52:06 -0500

    feat: add wrapper bash script

---
+++ merge_results.sh
@@ -0,0 +1,6 @@
+python3 merge_results.py /scratch/nn1331/entailment/data-multi_news.csv /scratch/nn1331/entailment/data-roberta-multi_news.csv /scratch/nn1331/entailment/merged-multi_news.csv
+python3 merge_results.py /scratch/nn1331/entailment/data-pile_books3.csv /scratch/nn1331/entailment/data-roberta-pile_books3.csv /scratch/nn1331/entailment/merged-pile_books3.csv
+python3 merge_results.py /scratch/nn1331/entailment/data-pile_wikipedia.csv /scratch/nn1331/entailment/data-roberta-pile_wikipedia.csv /scratch/nn1331/entailment/merged-pile_wikipedia.csv
+python3 merge_results.py /scratch/nn1331/entailment/data-reuters21578.csv /scratch/nn1331/entailment/data-roberta-reuters21578.csv /scratch/nn1331/entailment/merged-reuters21578.csv
+python3 merge_results.py /scratch/nn1331/entailment/data-yahoo_answers_topics.csv /scratch/nn1331/entailment/data-roberta-yahoo_answers_topics.csv /scratch/nn1331/entailment/merged-yahoo_answers_topics.csv
+python3 merge_results.py /scratch/nn1331/entailment/data-yelp_review_full.csv /scratch/nn1331/entailment/data-roberta-yelp_review_full.csv /scratch/nn1331/entailment/merged-yelp_review_full.csv
